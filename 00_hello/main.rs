980ecdd4930b7bd2365cdff58a7bb4df108b8a9c
Omar Crosby <omar.crosby@gmail.com>
2025-05-27 22:29:49 -0400

    chore: add hello world program

---
+++ 00_hello/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+    // This is a simple Rust program that prints "Hello, world!" to the console.
+    // You can run this code using `cargo run` in your terminal.
+}
