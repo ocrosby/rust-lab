026b98d0abdfaba461cbbb0e05dd8183dc5d6c2d
Omar Crosby <omar.crosby@gmail.com>
2025-06-09 06:56:34 -0400

    chore: delete duplicate hello project

--- 00_hello/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-    // This is a simple Rust program that prints "Hello, world!" to the console.
-    // You can run this code using `cargo run` in your terminal.
-}
-
